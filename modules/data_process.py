d02d5e5f4e1704ac9f6c8d02d9439dc6f049b9f2
joey <t1616joy@yahoo.com.tw>
2023-09-28 17:21:53 +0800

    remove Instrument tmp

--- modules/data_process.py
+++ modules/data_process.py
@@ -15,7 +15,7 @@ class SACProcess():
         self.sac_path = sac_path
         self.instrument_path = instrument_path
 
-    def readSACFile(self, sta): # TODO
+    def readSACFile(self, sta):
         total_files = glob.glob(f"{self.sac_path}/*{sta}*HLE*.SAC")
         file_names = [os.path.basename(_) for _ in total_files]
         return file_names
@@ -43,13 +43,12 @@ class SACProcess():
         os.rename(f"{path}/{file_name}", f"{path}/{new_file_name}")
         return new_file_name
     
-    def removeInstrumentResponse(self, sac_files, instrument, output):
-        for sac_file in sac_files:
-            s = f"r {self.sac_path}/{sac_file} \n"
-            s += "rmean; rtrend \n"
-            s += "taper \n"
-            s += f"trans from polezero s {instrument} to acc freq 0.02 0.1 1 10 \n"
-            s += f"w {output} \n"
+    def removeInstrumentResponse(self, sac_file, instrument):
+        s = f"r {self.sac_path}/rowdata/{sac_file} \n"
+        s += "rmean; rtrend \n"
+        s += "taper \n"
+        s += f"trans from polezero s {instrument} to acc freq 0.02 0.1 1 10 \n"
+        s += f"w {self.sac_path}/{sac_file} \n"
 
 class catalogProcess():
 
@@ -115,7 +114,6 @@ class catalogProcess():
     #     new_eqtime = new_date + new_time
     #     return new_eqtime
 
-
 class recordProcess():
 
     def __init__(self):
@@ -145,10 +143,62 @@ if __name__ == '__main__':
     sac_path = "../TSMIP_Dataset/GuanshanChishangeq"
     instrument_path = "../TSMIP_Dataset/InstrumentResponse"
     sac_process = SACProcess(sac_path, instrument_path)
-    sac_files = sac_process.readSACFile("A002")
-    instrument_files = sac_process.readInstrumentFile("A002")
-    output = "output.sac"
-    sac_process.removeInstrumentResponse(sac_files, instrument_files, output)
+    stations = [
+            'A002', 'A003', 'A004', 'A007', 'A008', 'A009', 'A010', 'A013',
+            'A014', 'A015', 'A016', 'A020', 'A024', 'A025', 'A026', 'A030',
+            'A032', 'A034', 'A036', 'A037', 'A039', 'A043', 'A044', 'A046',
+            'A049', 'A051', 'A052', 'A054', 'A057', 'A059', 'A060', 'A061',
+            'A063', 'A065', 'A066', 'A070', 'A071', 'A076', 'A077', 'A078',
+            'A079', 'A082', 'A083', 'A084', 'A085', 'A103', 'A107', 'A112',
+            'A115', 'A124', 'A125', 'A127', 'A128', 'A130', 'A131', 'A134',
+            'B006', 'B011', 'B012', 'B013', 'B014', 'B016', 'B017', 'B018',
+            'B019', 'B021', 'B022', 'B023', 'B024', 'B026', 'B027', 'B028',
+            'B029', 'B030', 'B033', 'B034', 'B035', 'B036', 'B037', 'B039',
+            'B041', 'B043', 'B045', 'B048', 'B049', 'B051', 'B052', 'B053',
+            'B059', 'B060', 'B061', 'B062', 'B064', 'B066', 'B068', 'B069',
+            'B070', 'B071', 'B073', 'B077', 'B078', 'B081', 'B082', 'B084',
+            'B085', 'B086', 'B090', 'B095', 'B097', 'B099', 'B103', 'B104',
+            'B107', 'B110', 'B111', 'B112', 'B115', 'B117', 'B118', 'B120',
+            'B121', 'B123', 'B127', 'B128', 'B129', 'B131', 'B135', 'B136',
+            'B138', 'B139', 'B143', 'B145', 'B149', 'B162', 'B168', 'B170',
+            'B171', 'B172', 'B173', 'B174', 'B175', 'B176', 'B177', 'B178',
+            'B179', 'B180', 'B181', 'B182', 'B184', 'B189', 'B190', 'B200',
+            'B201', 'B204', 'B207', 'B208', 'B209', 'B210', 'B215', 'B216',
+            'C001', 'C003', 'C004', 'C005', 'C006', 'C008', 'C010', 'C012',
+            'C014', 'C015', 'C016', 'C017', 'C021', 'C022', 'C023', 'C024',
+            'C026', 'C027', 'C029', 'C032', 'C034', 'C035', 'C037', 'C041',
+            'C043', 'C044', 'C045', 'C047', 'C049', 'C051', 'C053', 'C055',
+            'C056', 'C058', 'C060', 'C061', 'C062', 'C064', 'C065', 'C066',
+            'C069', 'C073', 'C074', 'C075', 'C076', 'C077', 'C078', 'C082',
+            'C084', 'C085', 'C087', 'C088', 'C092', 'C093', 'C094', 'C095',
+            'C097', 'C098', 'C099', 'C100', 'C102', 'C104', 'C105', 'C106',
+            'C107', 'C112', 'C113', 'C114', 'C116', 'C118', 'C121', 'C123',
+            'C124', 'C134', 'C137', 'C138', 'C139', 'C140', 'C141', 'C142',
+            'C143', 'C144', 'C145', 'C146', 'C150', 'C152', 'C155', 'C156',
+            'C157', 'C160', 'C161', 'C162', 'D005', 'D008', 'D009', 'D011',
+            'D012', 'D014', 'D015', 'D017', 'D023', 'D028', 'D029', 'D031',
+            'D032', 'D033', 'D034', 'D035', 'D039', 'D042', 'D044', 'D046',
+            'D047', 'D048', 'D049', 'D050', 'D051', 'D054', 'D060', 'D062',
+            'D063', 'D064', 'D065', 'D066', 'D067', 'D068', 'D069', 'D071',
+            'D072', 'D074', 'D075', 'D076', 'D077', 'D079', 'D084', 'D086',
+            'D088', 'D089', 'D090', 'D091', 'D097', 'D103', 'D104', 'D105',
+            'D106', 'D107', 'D108', 'D109', 'D110', 'D111', 'D112', 'D113',
+            'D114', 'D115', 'D117', 'D120', 'D122', 'D123', 'D126', 'E004',
+            'E006', 'E015', 'E022', 'E023', 'E026', 'E033', 'E034', 'E035',
+            'E037', 'E042', 'E046', 'E049', 'E050', 'E053', 'E059', 'E060',
+            'E061', 'E062', 'E067', 'E068', 'E069', 'E075', 'E076', 'F002',
+            'F004', 'F015', 'F019', 'F020', 'F026', 'F028', 'F036', 'F041',
+            'F042', 'F043', 'F044', 'F045', 'F048', 'F053', 'F054', 'F058',
+            'F067', 'F068', 'F071', 'F072', 'F073', 'F074', 'F075', 'G001',
+            'G002', 'G003', 'G014', 'G015', 'G016', 'G017', 'G020', 'G021',
+            'G022', 'G023', 'G025', 'G026', 'G028', 'G030', 'G032', 'G033',
+            'G035', 'G036', 'G037', 'G038', 'G041', 'G045', 'G047', 'G048',
+            'G052', 'G053', 'G055', 'G057', 'G060', 'G061', 'I002', 'J001'
+        ]
+    for sta in stations:
+        sac_files = sac_process.readSACFile(sta)
+        instrument_files = sac_process.readInstrumentFile(sta)
+        sac_process.removeInstrumentResponse(sac_files, instrument_files)
     # file_names = [sac_process.reName(path, os.path.basename(file)) for file in files]
 
     # catalogProcess
