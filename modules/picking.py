7e1ac7ea56122c122398ffbe6d0a59dfb4a34763
joey <t1616joy@yahoo.com.tw>
2023-10-14 14:27:45 +0800

    write output

--- modules/picking.py
+++ modules/picking.py
@@ -49,18 +49,18 @@ class picking():
         """
             To determine the output by Y or Z or 1-5
             Input: sac_file_name= "TW.A002.10.HLE.D.20220918144415.SAC"
-            Outupt: self.result & self.zory
+            Outupt: self.result: {1: ['TW.A002.10.HLE.D.20220918144415.SAC', 'y', 443.98, 118, 173, 202]}
+                    self.zory : "y"
         """
         logger.info("Accept [Y/y] or Accpet but Z [Z/z] or Reject [Others]?")
         check = input()
         if check == "Y" or check == "y":
             logger.info(f"Result : {check}")
-            # 存取資訊
-            self.result[num] = [sac_file_name, "y", round(sac1.t4,2), round(sac1.t3), round(sac1.t1) , round(sac1.t2)]
+            self.result[num] = [sac_file_name, "y", round(sac1.t4,2), round(sac1.t1) , round(sac1.t2), round(sac1.t3)]
             self.zory = "y"
         elif check == "Z" or check == "z":
             logger.info(f"Result : {check}")
-            self.result[num] = [sac_file_name, "z", round(sac1.t4,2), round(sac1.t3), round(sac1.t1) , round(sac1.t2)]
+            self.result[num] = [sac_file_name, "z", round(sac1.t4,2), round(sac1.t1) , round(sac1.t2), round(sac1.t3)]
             self.zory = "z"
         elif check == "1":
             logger.info(f"Result : 1")
@@ -89,12 +89,13 @@ class picking():
         data.__call__(asc_path)
         os.rename(f'{asc_path}/data.asc', f'{asc_path}/{file_name}.asc')
 
-    def dropDuplicate(self, df):
-        # 保留最後的定義
-        df = df.drop_duplicates(subset=[0],keep='last', inplace=False)
-        # 將資料做排序
-        df = df.sort_values(by=[0],ignore_index = True)
-        df.to_csv(f"{self.asc_path}/result.csv",header=False,index=False,mode='w')
+    def dropDuplicate(self, result):
+        """
+            To drop duplicate data in the result csv
+        """
+        result = result.drop_duplicates(subset=["file_name"],keep='last', inplace=False)
+        result = result.sort_values(by=["file_name"],ignore_index = True)
+        result.to_csv(f"{self.asc_path}/result.csv",index=False,mode='w')
 
     def mainProcess(self, sac_file_names, num):
         """
@@ -120,7 +121,7 @@ class picking():
                 s += "w over \n"
                 s += "q \n"
                 subprocess.Popen(['sac'], stdin=subprocess.PIPE).communicate(
-                    s.encode())  # show the interactivate window
+                    s.encode())
 
                 sac1 = SACTrace.read(f"{self.sac_path}/{sac_HLE}")
                 sac2 = SACTrace.read(f"{self.sac_path}/{sac_HLN}")
@@ -128,18 +129,23 @@ class picking():
                 # print(type(sac1.data[1]))
                 # print(sac1.reftime)
 
-                # self.inputResult(sac_file_name, sac1, sac2, sacZ)
-                # if self.zory != '':
-                #     self.sacToAsc(sac_file_name, sac1, sac2, sacZ, self.zory)
-                # logger.info(self.result)
+                self.inputResult(sac_file_name, sac1, sac2, sacZ)
+                if self.zory != '':
+                    self.sacToAsc(sac_file_name, sac1, sac2, sacZ, self.zory)
+                logger.info(self.result)
 
         finally:
-            df = pd.DataFrame.from_dict(self.result,orient='index')
-            # 轉成csv並加在原有的後面
-            df.to_csv(f"{self.asc_path}/result.csv",header=False,index=True,mode='a') 
-            df = pd.read_csv(f"{self.asc_path}/result.csv",header=None)
-            _ = self.dropDuplicate(df)
-            logger.info("finish output!!")
+            file_exists = os.path.exists(f"{self.asc_path}/result.csv")
+            result = pd.DataFrame.from_dict(self.result,orient='index')
+            if not file_exists:
+                columns = ["file_name","pick_result","start_time","p_arrival","s_arrival","end_time"]
+                result.to_csv(f"{self.asc_path}/result.csv",header=columns,index=True,mode='a')
+                logger.info("finish add result.csv.")
+            else:
+                result.to_csv(f"{self.asc_path}/result.csv",header=False,index=True,mode='a')
+                check_result = pd.read_csv(f"{self.asc_path}/result.csv")
+                _ = self.dropDuplicate(check_result)
+                logger.info("finish append result.csv.")
 
 if __name__ == '__main__':
     year = "2022"
