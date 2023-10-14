ac71a97553111125c89264f378f26bc26d058eb9
joey <t1616joy@yahoo.com.tw>
2023-10-14 12:12:52 +0800

    step4 5 merge sta dist and predict arrival

--- modules/data_process.py
+++ modules/data_process.py
@@ -142,16 +142,27 @@ class catalogProcess():
 
 class recordProcess():
 
-    def __init__(self, catalog):
+    def __init__(self, catalog, stations):
         self.catalog = catalog
+        self.stations = stations
     
-    def getDistance(self, records, stations):
+    def getDistance(self, records):
+        """
+            To calculate the distance between the station and earthquake
+            Input : records = Dataframe records csv
+            Output : the sta_dist column series
+        """
         tmp = pd.merge(records, self.catalog, on='event_id', how='inner')
-        result = pd.merge(tmp, stations, on='station', how='inner')
+        result = pd.merge(tmp, self.stations, on='station', how='inner')
         result["sta_dist"] = (((result["longitude"]-result["lon"])*101.7)**2 +  ((result["latitude"]-result["lat"])*110.9)**2 +  (result["depth"]-result["dep"])**2)**(1/2)
         return result["sta_dist"]
 
     def getArrivalTime(self, records):
+        """
+            To calculate the arrival time through iasp91 method
+            Input : records = Dataframe records csv
+            Output : list P_arrival and S_arrival 
+        """
         result = pd.merge(records, self.catalog, on='event_id', how='inner')
         iasp91_P_arrival = []
         iasp91_S_arrival = []
@@ -191,7 +202,7 @@ class recordProcess():
 
 if __name__ == '__main__':
 
-    # SACProcess
+    #! SACProcess
     sac_path = "../TSMIP_Dataset/GuanshanChishangeq"
     instrument_path = f"../TSMIP_Dataset/InstrumentResponse/All"
     sac_process = SACProcess(sac_path, instrument_path)
@@ -204,72 +215,29 @@ if __name__ == '__main__':
     # step-2 rename
     # file_names = [sac_process.reName(sac_path, os.path.basename(file)) for file in sac_files]
 
-    # step-3 build record csv
-    # recordProcess
+    #! recordProcess
     catalog_path = "../TSMIP_Dataset/GDMS_catalog.csv"
-    record_path = "../TSMIP_Dataset/GDMS_Record.csv"
+    stations_path = "../TSMIP_Dataset/GDMS_stations.csv"
+    stations = pd.read_csv(stations_path)
     catalog = pd.read_csv(catalog_path)
-    record_process = recordProcess(catalog)
+    record_process = recordProcess(catalog, stations)
+    
+    # step-3 build record csv
+    record_path = "../TSMIP_Dataset/GDMS_Record.csv"
+    # sac_files = sac_process.getSACFile(get_all=False)
+    # record = record_process.getRecordDf(sac_files)
+    # _ = record_process.buildRecordFile(record, record_path)
 
-    sac_files = sac_process.getSACFile(get_all=False)
-    record = record_process.getRecordDf(sac_files)
-    _ = record_process.buildRecordFile(record, record_path)
-    stations = [
-        'A002', 'A003', 'A004', 'A007', 'A008', 'A009', 'A010', 'A013', 'A014',
-        'A015', 'A016', 'A020', 'A024', 'A025', 'A026', 'A030', 'A032', 'A034',
-        'A036', 'A037', 'A039', 'A043', 'A044', 'A046', 'A049', 'A051', 'A052',
-        'A054', 'A057', 'A059', 'A060', 'A061', 'A063', 'A065', 'A066', 'A070',
-        'A071', 'A076', 'A077', 'A078', 'A079', 'A082', 'A083', 'A084', 'A085',
-        'A103', 'A107', 'A112', 'A115', 'A124', 'A125', 'A127', 'A128', 'A130',
-        'A131', 'A134', 'B006', 'B011', 'B012', 'B013', 'B014', 'B016', 'B017',
-        'B018', 'B019', 'B021', 'B022', 'B023', 'B024', 'B026', 'B027', 'B028',
-        'B029', 'B030', 'B033', 'B034', 'B035', 'B036', 'B037', 'B039', 'B041',
-        'B043', 'B045', 'B048', 'B049', 'B051', 'B052', 'B053', 'B059', 'B060',
-        'B061', 'B062', 'B064', 'B066', 'B068', 'B069', 'B070', 'B071', 'B073',
-        'B077', 'B078', 'B081', 'B082', 'B084', 'B085', 'B086', 'B090', 'B095',
-        'B097', 'B099', 'B103', 'B104', 'B107', 'B110', 'B111', 'B112', 'B115',
-        'B117', 'B118', 'B120', 'B121', 'B123', 'B127', 'B128', 'B129', 'B131',
-        'B135', 'B136', 'B138', 'B139', 'B143', 'B145', 'B149', 'B162', 'B168',
-        'B170', 'B171', 'B172', 'B173', 'B174', 'B175', 'B176', 'B177', 'B178',
-        'B179', 'B180', 'B181', 'B182', 'B184', 'B189', 'B190', 'B200', 'B201',
-        'B204', 'B207', 'B208', 'B209', 'B210', 'B215', 'B216', 'C001', 'C003',
-        'C004', 'C005', 'C006', 'C008', 'C010', 'C012', 'C014', 'C015', 'C016',
-        'C017', 'C021', 'C022', 'C023', 'C024', 'C026', 'C027', 'C029', 'C032',
-        'C034', 'C035', 'C037', 'C041', 'C043', 'C044', 'C045', 'C047', 'C049',
-        'C051', 'C053', 'C055', 'C056', 'C058', 'C060', 'C061', 'C062', 'C064',
-        'C065', 'C066', 'C069', 'C073', 'C074', 'C075', 'C076', 'C077', 'C078',
-        'C082', 'C084', 'C085', 'C087', 'C088', 'C092', 'C093', 'C094', 'C095',
-        'C097', 'C098', 'C099', 'C100', 'C102', 'C104', 'C105', 'C106', 'C107',
-        'C112', 'C113', 'C114', 'C116', 'C118', 'C121', 'C123', 'C124', 'C134',
-        'C137', 'C138', 'C139', 'C140', 'C141', 'C142', 'C143', 'C144', 'C145',
-        'C146', 'C150', 'C152', 'C155', 'C156', 'C157', 'C160', 'C161', 'C162',
-        'D005', 'D008', 'D009', 'D011', 'D012', 'D014', 'D015', 'D017', 'D023',
-        'D028', 'D029', 'D031', 'D032', 'D033', 'D034', 'D035', 'D039', 'D042',
-        'D044', 'D046', 'D047', 'D048', 'D049', 'D050', 'D051', 'D054', 'D060',
-        'D062', 'D063', 'D064', 'D065', 'D066', 'D067', 'D068', 'D069', 'D071',
-        'D072', 'D074', 'D075', 'D076', 'D077', 'D079', 'D084', 'D086', 'D088',
-        'D089', 'D090', 'D091', 'D097', 'D103', 'D104', 'D105', 'D106', 'D107',
-        'D108', 'D109', 'D110', 'D111', 'D112', 'D113', 'D114', 'D115', 'D117',
-        'D120', 'D122', 'D123', 'D126', 'E004', 'E006', 'E015', 'E022', 'E023',
-        'E026', 'E033', 'E034', 'E035', 'E037', 'E042', 'E046', 'E049', 'E050',
-        'E053', 'E059', 'E060', 'E061', 'E062', 'E067', 'E068', 'E069', 'E075',
-        'E076', 'F002', 'F004', 'F015', 'F019', 'F020', 'F026', 'F028', 'F036',
-        'F041', 'F042', 'F043', 'F044', 'F045', 'F048', 'F053', 'F054', 'F058',
-        'F067', 'F068', 'F071', 'F072', 'F073', 'F074', 'F075', 'G001', 'G002',
-        'G003', 'G014', 'G015', 'G016', 'G017', 'G020', 'G021', 'G022', 'G023',
-        'G025', 'G026', 'G028', 'G030', 'G032', 'G033', 'G035', 'G036', 'G037',
-        'G038', 'G041', 'G045', 'G047', 'G048', 'G052', 'G053', 'G055', 'G057',
-        'G060', 'G061', 'I002', 'J001'
-    ]
-    # records = pd.read_csv(f"{path}/{output_name}")
-    # stations_name = "GDMS_stations.csv"
-    # stations = pd.read_csv(f"{path}/{stations_name}")
-    # result = record_process.getDistance(catalog, records, stations)
-    # records["sta_dist"] = result
-    # iasp91_P_arrival, iasp91_S_arrival = record_process.getArrivalTime(catalog, records)
-    # records["iasp91_P_arrival"] = iasp91_P_arrival
-    # records["iasp91_S_arrival"] = iasp91_S_arrival
-    # _ = record_process.buildRecordFile(records, path+output_name)
+    # step-4 merge Distance
+    records = pd.read_csv(record_path)
+    result = record_process.getDistance(records)
+    records["sta_dist"] = result
+
+    # step-5 merge P S arrival
+    iasp91_P_arrival, iasp91_S_arrival = record_process.getArrivalTime(records)
+    records["iasp91_P_arrival"] = iasp91_P_arrival
+    records["iasp91_S_arrival"] = iasp91_S_arrival
+    _ = record_process.buildRecordFile(records, record_path)
 
     # catalogProcess
     # path = "../TSMIP_Dataset"
