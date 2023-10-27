4754c386fc3f43bd583a59d7a608f4b493bbc052
joey <t1616joy@yahoo.com.tw>
2023-10-27 16:01:20 +0800

    change to web search method

--- modules/GCMT_api.py
+++ modules/GCMT_api.py
@@ -19,18 +19,48 @@ class GCMT():
     def __init__(self):
         self.api_url = config.get("GCMT_api_url")
         self.output_path = config.get("download_path")
-    
-    def getCatalog(self, year ,month):
-        get_catalog_url = f"{self.api_url}/{year}/{month}.ndk"
+
+    def getCatalog(self):
+        catalog_condition = config.get("gcmt_catalog_range")
+        itype = catalog_condition.get("itype")
+        yr = catalog_condition.get("yr")
+        mo = catalog_condition.get("mo")
+        day = catalog_condition.get("day")
+        otype = catalog_condition.get("otype")
+        oyr = catalog_condition.get("oyr")
+        omo = catalog_condition.get("omo")
+        oday = catalog_condition.get("oday")
+        jyr = catalog_condition.get("jyr")
+        jday = catalog_condition.get("jday")
+        ojyr = catalog_condition.get("ojyr")
+        ojday = catalog_condition.get("ojday")
+        nday = catalog_condition.get("nday")
+        lmw = catalog_condition.get("lmw")
+        umw = catalog_condition.get("umw")
+        lms = catalog_condition.get("lms")
+        ums = catalog_condition.get("ums")
+        lmb = catalog_condition.get("lmb")
+        umb = catalog_condition.get("umb")
+        llat = catalog_condition.get("llat")
+        ulat = catalog_condition.get("ulat")
+        llon = catalog_condition.get("llon")
+        ulon = catalog_condition.get("ulon")
+        lhd = catalog_condition.get("lhd")
+        uhd = catalog_condition.get("uhd")
+        lts = catalog_condition.get("lts")
+        uts = catalog_condition.get("uts")
+        lpe1 = catalog_condition.get("lpe1")
+        upe1 = catalog_condition.get("upe1")
+        lpe2 = catalog_condition.get("lpe2")
+        upe2 = catalog_condition.get("upe2")
+        list = catalog_condition.get("list")
+        get_catalog_url = f"{self.api_url}/cgi-bin/globalcmt-cgi-bin/CMT5/form?itype={itype}&yr={yr}&mo={mo}&day={day}&otype={otype}&oyr={oyr}&omo={omo}&oday={oday}&jyr={jyr}&jday={jday}&ojyr={ojyr}&ojday={ojday}&nday={nday}&lmw={lmw}&umw={umw}&lms={lms}&ums={ums}&lmb={lmb}&umb={umb}&llat={llat}&ulat={ulat}&llon={llon}&ulon={ulon}&lhd={lhd}&uhd={uhd}&lts={lts}&uts={uts}&lpe1={lpe1}&upe1={upe1}&lpe2={lpe2}&upe2={upe2}&list={list}"
+
         cat = requests.get(get_catalog_url)
-        # cat = obspy.read_events(get_catalog_url)
         return cat
 
 
 if __name__ == '__main__':
     gcmt = GCMT()
-    cat = gcmt.getCatalog(2023,"mar23")
+    cat = gcmt.getCatalog()
     print(cat)
-    with open("test.ndk","w") as f:
-        f.write(cat.text)
-    ccc=csep.load_catalog("test.ndk")
