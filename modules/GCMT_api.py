6d54cbc72655844f15066d665fec44d8a1bec366
joey <t1616joy@yahoo.com.tw>
2023-10-27 20:30:59 +0800

    get eq infomation from GCMT

--- modules/GCMT_api.py
+++ modules/GCMT_api.py
@@ -59,8 +59,79 @@ class GCMT():
         cat = requests.get(get_catalog_url)
         return cat
 
+    def parserGCMT(self, cat):
+        parser = BeautifulSoup(cat, 'html.parser')
+        gcmt_info = parser.find_all("pre")
+        date = []
+        centroid_time = []
+        lat = []
+        lon = []
+        depth = []
+        half_duration = []
+        centroid_time_minus_hypocenter_time = []
+        mw = []
+        mb = []
+        ms = []
+        scalar_moment = []
+        strike1 = []
+        dip1 = []
+        slip1 = []
+        strike2 = []
+        dip2 = []
+        slip2 = []
+        for i in gcmt_info[1:]:
+            date_match = re.search(r"Date:\s*(\d{4}/\s*\d{1,2}/\s*\d{1,2})",
+                                   i.text)
+            centroid_time_match = re.search(
+                r"Centroid Time:\s*(\d{1,2}:\d{2}:\d{2})", i.text)
+            lat_match = re.search(r"Lat=\s*([-\d\.]+)", i.text)
+            lon_match = re.search(r"Lon=\s*([-\d\.]+)", i.text)
+            depth_match = re.search(r"Depth=\s*([\d\.]+)", i.text)
+            half_duration_match = re.search(r"Half duration=\s*([\d\.]+)",
+                                            i.text)
+            centroid_time_minus_hypocenter_time_match = re.search(
+                r"Centroid time minus hypocenter time:\s*(\d{1,2}\.\d{1,2})",
+                i.text)
+            mw_match = re.search(r"Mw =\s*([\d\.]+)", i.text)
+            mb_match = re.search(r"mb =\s*([\d\.]+)", i.text)
+            ms_match = re.search(r"Ms =\s*([\d\.]+)", i.text)
+            scalar_moment_match = re.search(r"Scalar Moment =\s*([\d\.e\+]+)",
+                                            i.text)
+            strike1_match = re.findall(r"strike=([-\d\.]+)", i.text)[0]
+            dip1_match = re.findall(r"dip=\s*([-\d\.]+)", i.text)[0]
+            slip1_match = re.findall(r"slip=\s*([-\d\.]+)", i.text)[0]
+            strike2_match = re.findall(r"strike=([-\d\.]+)", i.text)[1]
+            dip2_match = re.findall(r"dip=\s*([-\d\.]+)", i.text)[1]
+            slip2_match = re.findall(r"slip=\s*([-\d\.]+)", i.text)[1]
+
+            date.append(date_match.group(1)) if date_match else None
+            centroid_time.append(
+                centroid_time_match.group(1)) if centroid_time_match else None
+            lat.append(lat_match.group(1)) if lat_match else None
+            lon.append(lon_match.group(1)) if lon_match else None
+            depth.append(depth_match.group(1)) if depth_match else None
+            half_duration.append(
+                half_duration_match.group(1)) if half_duration_match else None
+            centroid_time_minus_hypocenter_time.append(
+                centroid_time_minus_hypocenter_time_match.group(
+                    1)) if centroid_time_minus_hypocenter_time_match else None
+            mw.append(mw_match.group(1)) if mw_match else None
+            mb.append(mb_match.group(1)) if mb_match else None
+            ms.append(ms_match.group(1)) if ms_match else None
+            scalar_moment.append(
+                scalar_moment_match.group(1)) if scalar_moment_match else None
+            strike1.append(strike1_match) if strike1_match else None
+            dip1.append(dip1_match) if dip1_match else None
+            slip1.append(slip1_match) if slip1_match else None
+            strike2.append(strike2_match) if strike2_match else None
+            dip2.append(dip2_match) if dip2_match else None
+            slip2.append(slip2_match) if slip2_match else None
+
+        return date, centroid_time, lat, lon, depth, half_duration, centroid_time_minus_hypocenter_time, mw, mb, ms, scalar_moment, strike1, dip1, slip1, strike2, dip2, slip2
+
 
 if __name__ == '__main__':
     gcmt = GCMT()
     cat = gcmt.getCatalog()
-    print(cat)
+    date, centroid_time, lat, lon, depth, half_duration, centroid_time_minus_hypocenter_time, mw, mb, ms, scalar_moment, strike1, dip1, slip1, strike2, dip2, slip2 = gcmt.parserGCMT(cat.text)
+    print(mw)
